25994e49bed3712c958355cfef7a28ae77c41dd7
869123771 <869123771@qq.com>
2019-06-13 19:39:36 +0800

    modify user

--- src/views/system/UserList.vue
+++ src/views/system/UserList.vue
@@ -1,38 +1,38 @@
 <template>
-    <div class = "user">
+    <div class="user">
         <el-row>
-            <el-form ref = "form" :model = "form" label-width="80px">
+            <el-form ref="form" :model="form" label-width="80px">
                 <el-row>
                     <el-col :md="6" :sm="8">
-                        <el-form-item label = "账号:" prop = "username">
-                            <el-input v-model = "form.username" clearable></el-input>
+                        <el-form-item label="账号:" prop="username">
+                            <el-input v-model="form.username" clearable></el-input>
                         </el-form-item>
                     </el-col>
                     <el-col :md="6" :sm="8">
-                        <el-form-item label = "性别:" prop = "sex">
-                            <el-select v-model = "form.sex" clearable filterable class = "w-full">
-                                <template v-for = "item in sex">
-                                    <el-option :value = "item.itemValue" :label = "item.itemText"></el-option>
+                        <el-form-item label="性别:" prop="sex">
+                            <el-select v-model="form.sex" clearable filterable class="w-full">
+                                <template v-for="item in sex">
+                                    <el-option :value="item.itemValue" :label="item.itemText"></el-option>
                                 </template>
                             </el-select>
                         </el-form-item>
                     </el-col>
-                    <div v-show = "show.collapse">
+                    <div v-show="show.collapse">
                         <el-col :md="6" :sm="8">
-                            <el-form-item label = "邮箱:" prop = "email">
-                                <el-input v-model = "form.email" clearable></el-input>
+                            <el-form-item label="邮箱:" prop="email">
+                                <el-input v-model="form.email" clearable></el-input>
                             </el-form-item>
                         </el-col>
                         <el-col :md="6" :sm="8">
-                            <el-form-item label = "手机号码:" prop = "phone">
-                                <el-input v-model = "form.phone" clearable></el-input>
+                            <el-form-item label="手机号码:" prop="phone">
+                                <el-input v-model="form.phone" clearable></el-input>
                             </el-form-item>
                         </el-col>
                         <el-col :md="6" :sm="8">
-                            <el-form-item label = "状态:" prop = "status">
-                                <el-select v-model = "form.status" clearable filterable class = "w-full">
-                                    <template v-for = "item in userStatus">
-                                        <el-option :value = "item.itemValue" :label = "item.itemText"></el-option>
+                            <el-form-item label="状态:" prop="status">
+                                <el-select v-model="form.status" clearable filterable class="w-full">
+                                    <template v-for="item in userStatus">
+                                        <el-option :value="item.itemValue" :label="item.itemText"></el-option>
                                     </template>
                                 </el-select>
                             </el-form-item>
@@ -40,16 +40,16 @@
                     </div>
                     <el-col :md="6" :sm="8">
                         <el-form-item label-width="38px">
-                            <el-button type = "primary" icon="el-icon-search" @click = "search">查询</el-button>
-                            <el-button plain icon = "el-icon-refresh-left" @click = "reset">重置</el-button>
-                            <span class = "cursor-pointer inline-block text-blue-500 pl-2" @click = "arrowClick">
-                            <span v-if = "!show.collapse">
+                            <el-button type="primary" icon="el-icon-search" @click="search">查询</el-button>
+                            <el-button plain icon="el-icon-refresh-left" @click="reset">重置</el-button>
+                            <span class="cursor-pointer inline-block text-blue-500 pl-2" @click="arrowClick">
+                            <span v-if="!show.collapse">
                                 <span>展开</span>
-                                <i class = "el-icon-arrow-down pl-1"></i>
+                                <i class="el-icon-arrow-down pl-1"></i>
                             </span>
                             <span v-else>
                                 <span>收起</span>
-                                <i class = "el-icon-arrow-up pl-1"></i>
+                                <i class="el-icon-arrow-up pl-1"></i>
                             </span>
                         </span>
                         </el-form-item>
@@ -57,17 +57,17 @@
                 </el-row>
             </el-form>
         </el-row>
-        <el-row class = "my-3">
+        <el-row class="my-3">
             <el-button plain icon="iconfont icon-upload">导入</el-button>
             <el-button plain icon="iconfont icon-download">导出</el-button>
-            <el-dropdown placement="bottom" class = "dropdown" v-show = "show.batch">
+            <el-dropdown placement="bottom" class="dropdown" v-show="show.batch">
                 <el-button plain>
                     批量操作<i class="el-icon-arrow-down el-icon--right"></i>
                 </el-button>
                 <el-dropdown-menu slot="dropdown">
-                    <el-dropdown-item><i class = "el-icon-delete"></i>删除</el-dropdown-item>
-                    <el-dropdown-item><i class = "el-icon-lock"></i>冻结</el-dropdown-item>
-                    <el-dropdown-item><i class = "el-icon-unlock"></i>解冻</el-dropdown-item>
+                    <el-dropdown-item><i class="el-icon-delete"></i>删除</el-dropdown-item>
+                    <el-dropdown-item><i class="el-icon-lock"></i>冻结</el-dropdown-item>
+                    <el-dropdown-item><i class="el-icon-unlock"></i>解冻</el-dropdown-item>
                 </el-dropdown-menu>
             </el-dropdown>
         </el-row>
@@ -78,8 +78,8 @@
                     fit
                     align="center"
                     v-loading="table.loading"
-                    :column = "table.column"
-                    :data = "table.data"
+                    :column="table.column"
+                    :data="table.data"
                     pagination
                     background
                     layout="total, sizes, prev, pager, next, jumper"
@@ -97,40 +97,65 @@
 </template>
 
 <script>
-    import {mapState,mapActions} from 'vuex'
-    import {http,apiList,constant} from '@/utils'
+    import {mapState, mapActions} from 'vuex'
+    import {http, apiList, constant} from '@/utils'
     import lbTable from '@/components/lb-table/lb-table'
+
     export default {
         name: "UserList",
-        components : {
+        components: {
             lbTable
         },
-        data(){
+        data() {
             return {
-                form : {
-                    username : '',
-                    sex : '',
-                    email : '',
-                    phone : '',
-                    status : ''
+                form: {
+                    username: '',
+                    sex: '',
+                    email: '',
+                    phone: '',
+                    status: ''
                 },
-                show : {
-                    collapse : false,
-                    batch : false,
+                show: {
+                    collapse: false,
+                    batch: false,
                 },
-                table : {
-                    column : [
-                        {type : 'selection',fixed : true},
-                        {label : '用户账号',prop : 'username',},
-                        {label : '真实姓名',prop : 'realname'},
+                table: {
+                    column: [
+                        {type: 'selection', fixed: true},
                         {
-                            label : '头像',
-                            width : '70',
-                            prop : 'avatar',
-                            render : (h,scope)=>{
+                            label: '操作',
+                            prop: 'oper',
+                            width: '100',
+                            render: (h, props) => {
+                                let {row} = props
+                                return (
+                                    <el-dropdown placement="bottom" className="dropdown">
+                                      <span class="text-blue-500">
+                                          更多<i class="el-icon-arrow-down el-icon--right"></i>
+                                      </span>
+                                        <el-dropdown-menu slot="dropdown">
+                                            <el-dropdown-item nativeOnClick={() => this.handleDetail(row)}>详情
+                                            </el-dropdown-item>
+                                            <el-dropdown-item nativeOnClick={() => this.handlePwd(row)}>密码
+                                            </el-dropdown-item>
+                                            <el-dropdown-item>删除</el-dropdown-item>
+                                            <el-dropdown-item>冻结</el-dropdown-item>
+                                            <el-dropdown-item>代理人</el-dropdown-item>
+                                        </el-dropdown-menu>
+                                    </el-dropdown>
+                                )
+                            },
+                        },
+                        {label: '用户账号', prop: 'username',},
+                        {label: '真实姓名', prop: 'realname'},
+                        {
+                            label: '头像',
+                            width: '70',
+                            prop: 'avatar',
+                            render: (h, scope) => {
                                 return (
                                     <div>
-                                        <span class = "p-1 bg-gray-400 rounded-full cursor-pointer">
+                                        <span class="p-1 bg-gray-400 rounded-sm cursor-pointer">
                                             <i class="iconfont icon-user"></i>
                                         </span>
                                     </div>
@@ -138,64 +163,70 @@
                             }
                         },
                         {
-                            label : '性别',
-                            prop : 'sex_dictText',
-                            width : '70'
+                            label: '性别',
+                            prop: 'sex_dictText',
+                            width: '70'
                         },
-                        {label : '生日',prop : 'birthday'},
-                        {label : '手机号码',prop : 'phone',showOverflowTooltip:true},
-                        {label : '邮箱',prop : 'email'},
-                        {label : '状态',prop : 'status_dictText',width : '70'},
+                        {label: '生日', prop: 'birthday'},
+                        {label: '手机号码', prop: 'phone', showOverflowTooltip: true},
+                        {label: '邮箱', prop: 'email'},
+                        {label: '状态', prop: 'status_dictText', width: '70'},
                     ],
-                    data : [],
-                    selection : [],
-                    loading : false
+                    data: [],
+                    selection: [],
+                    loading: false
                 },
-                page : {
-                    currentPage : 1,
-                    pageSize : 10,
-                    total : 0
+                page: {
+                    currentPage: 1,
+                    pageSize: 10,
+                    total: 0
                 }
             }
         },
-        computed : {
+        computed: {
             ...mapState({
-                sex : ({dict}) => dict.sex,
-                userStatus : ({dict}) => dict.userStatus
+                sex: ({dict}) => dict.sex,
+                userStatus: ({dict}) => dict.userStatus
             })
         },
-        methods : {
+        methods: {
             ...mapActions({
-                getSex : 'GET_SEX',
-                getUserStatus : 'GET_USER_STATUS',
+                getSex: 'GET_SEX',
+                getUserStatus: 'GET_USER_STATUS',
             }),
-            arrowClick(){
+            arrowClick() {
                 let {collapse} = this.show
                 this.show = {
                     ...this.show,
-                    collapse : !collapse
+                    collapse: !collapse
                 }
             },
-            search(){
+            handleDetail(row) {
+                debugger;
+            },
+            handlePwd(row) {
+                debugger;
+            },
+            search() {
                 this.page = {
                     ...this.page,
-                    currentPage : 1
+                    currentPage: 1
                 }
-               this.queryList()
+                this.queryList()
             },
-            reset(){
-               this.$refs.form.resetFields()
+            reset() {
+                this.$refs.form.resetFields()
             },
-            checked(selection){
-                if(selection.length){
+            checked(selection) {
+                if (selection.length) {
                     this.show = {
                         ...this.show,
-                        batch : true
+                        batch: true
                     }
-                }else{
+                } else {
                     this.show = {
                         ...this.show,
-                        batch : false
+                        batch: false
                     }
                 }
                 this.table = {
@@ -203,36 +234,36 @@
                     selection
                 }
             },
-            handleCurrentChange(currentPage){
+            handleCurrentChange(currentPage) {
                 this.page = {
                     ...this.page,
                     currentPage
                 }
                 this.queryList()
             },
-            handleSizeChange(pageSize){
-              this.page = {
-                  ...this.page,
-                  pageSize
-              }
-              this.queryList()
+            handleSizeChange(pageSize) {
+                this.page = {
+                    ...this.page,
+                    pageSize
+                }
+                this.queryList()
             },
-            async queryList(){
+            async queryList() {
                 this.table = {
                     ...this.table,
-                    loading : true
+                    loading: true
                 }
-                let {pageSize,currentPage:pageNo} = this.page
+                let {pageSize, currentPage: pageNo} = this.page
                 let params = {
                     ...this.form,
                     pageSize,
                     pageNo
                 }
-                let {success,result:{total,records:data}} = await http.get(apiList.sys_user_query_list,params)
-                if(success){
+                let {success, result: {total, records: data}} = await http.get(apiList.sys_user_query_list, params)
+                if (success) {
                     this.table = {
                         ...this.table,
-                        loading : false,
+                        loading: false,
                         data
                     }
                     this.page = {
@@ -242,7 +273,7 @@
                 }
             }
         },
-        mounted(){
+        mounted() {
             this.queryList()
             this.getSex()
             this.getUserStatus()
@@ -250,10 +281,10 @@
     }
 </script>
 
-<style scoped lang = "less">
-    .user{
-        .dropdown{
-            padding : 0 10px;
+<style scoped lang="less">
+    .user {
+        .dropdown {
+            padding: 0 10px;
         }
     }
 </style>
