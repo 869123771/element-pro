bba249f7aec93020d8af217d700601be6fd20c28
869123771 <869123771@qq.com>
2020-10-22 18:44:32 +0800

    Update CodeEditor.vue

--- src/views/jeecg/CodeEditor.vue
+++ src/views/jeecg/CodeEditor.vue
@@ -21,6 +21,20 @@
                     :height="editor.height"></editor>
         </el-row>-->
         <el-row class = "my-4">
+            <vxe-table
+                    keep-source
+                    :data="table.data"
+                    :tooltip-config="{enabled: true}"
+                    :sort-config="{iconAsc: 'fa fa-chevron-up', iconDesc: 'fa fa-chevron-down'}"
+                    :keyboard-config="{isArrow: true}"
+                    :checkbox-config="{trigger: 'row', highlight: false, range: false}"
+                    :edit-config="{trigger: 'click', mode: 'cell', showStatus: true}"
+                    :edit-rules="validRules"
+            >
+                <vxe-table-column v-for="(config, index) in table.column" :key="index" v-bind="config"></vxe-table-column>
+            </vxe-table>
+        </el-row>
+        <el-row class = "my-4">
             <vue-live
                     class = "live-editor"
                     :delay="2000"
@@ -100,6 +114,43 @@
                         //minLines : 6,
                         //maxLines : Infinity,
                     }
+                }
+            },
+            validRules: {
+                    name: [
+                        { required: true, message: '必填' },
+                    ],
+                },
+                table : {
+                    column : [
+                        { type: 'seq',title : '#', width: 60, fixed: null },
+                        { type: 'checkbox', width: 50, fixed: null },
+                        {
+                            field: 'name',
+                            title: 'Name',
+                            width: 200,
+                            editRender : {
+                                name : 'ElInput',
+                                props : {
+                                    clearable : true
+                                }
+                            }
+                        },
+                        { field: 'nickname', title: 'Nickname'},
+                        { field: 'sex', title: 'Sex', width: 200,sortable : true },
+                        { field: 'role', title: 'Role', width: 200 },
+                        { field: 'address', title: 'Address', width: 300, showOverflow: true }
+                    ],
+                     data : [
+                         { id: 10001, name: 'Test1', nickname: 'T1', role: 'Develop', sex: 'Man', age: 28, address: 'vxe-table 从入门到放弃aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa' },
+                         { id: 10002, name: 'Test2', nickname: 'T2', role: 'Test', sex: 'Women', age: 22, address: 'Guangzhou' },
+                         { id: 10003, name: 'Test3', nickname: 'T3', role: 'PM', sex: 'Man', age: 32, address: 'Shanghai' },
+                         { id: 10004, name: 'Test4', nickname: 'T4', role: 'Designer', sex: 'Women ', age: 23, address: 'vxe-table 从入门到放弃' },
+                         { id: 10005, name: 'Test5', nickname: 'T5', role: 'Develop', sex: 'Women ', age: 30, address: 'Shanghai' },
+                         { id: 10006, name: 'Test6', nickname: 'T6', role: 'Designer', sex: 'Women ', age: 21, address: 'vxe-table 从入门到放弃' },
+                         { id: 10007, name: 'Test7', nickname: 'T7', role: 'Test', sex: 'Man ', age: 29, address: 'vxe-table 从入门到放弃' },
+                         { id: 10008, name: 'Test8', nickname: 'T8', role: 'Develop', sex: 'Man ', age: 35, address: 'vxe-table 从入门到放弃' }
+                     ]
                 }
             }
         },
